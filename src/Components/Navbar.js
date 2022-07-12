c0b3577d089553029392af89e2392c43a898c17c
Aman Kr. Singh <aman986838@gmail.com>
2022-07-12 15:04:43 +0530

    responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,30 +1,98 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
+import { CgGitFork } from "react-icons/cg";
+import { CgFileDocument } from "react-icons/cg";
+import {
+  AiOutlineHome,
+  AiOutlineUser,
+  AiFillCar
+} from "react-icons/ai";
 
-export default function Header() {
+function Navbar(props) {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <header>
-      <div className='header-inner'>
-        <div className='logo'>CARS</div>
-        <nav>
-          <ul>
-            <li>
-              <a href='/'>Home</a>
+    <>
+      <nav className={`navbar nav-foot-${props.mode}`}>
+        <div className='navbar-container'>
+          <a href='/' className={`navbar-logo logo text`} onClick={closeMobileMenu}>
+            <AiFillCar style={{ marginRight: "4px" }} />  CARS
+          </a>
+          <div className={`menu-icon`} onClick={handleClick}>
+            <i className={click ? `fas fa-times text` : `fas fa-bars text`} />
+          </div>
+          <ul className={click ? `nav-menu active` : 'nav-menu'}>
+            <li className='nav-item'>
+              <a href='/' className={`nav-links text`} onClick={closeMobileMenu}>
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+              </a>
             </li>
-            <li>
-              <a href='/'>Products</a>
+            <li className='nav-item'>
+              <a
+                href='/aboutus'
+                className={`nav-links text`}
+                onClick={closeMobileMenu}
+              >
+                <AiOutlineUser style={{ marginBottom: "2px" }} /> About
+              </a>
             </li>
-            <li>
-              <a href='/'>About</a>
+            <li className='nav-item'>
+              <a
+                href='https://drive.google.com/file/d/1ScwZI6ChQHlEqSq5JtGDJhEKz7wQjoIP/view?usp=sharing'
+                target= "_blank"
+                rel="noreferrer"
+                className={`nav-links text`}
+                onClick={closeMobileMenu}
+              >
+                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
+              </a>
             </li>
+
             <li>
-              <a href='/'>Contact</a>
-            </li>
-            <li className='btn'>
-              <a href='/'>Buy Now</a>
+              <a
+                href='https://github.com/amanrathore48'
+                className={`nav-links-mobile text`}
+                onClick={closeMobileMenu}
+                target='_blank'
+                rel="noreferrer"
+              >
+                <CgGitFork style={{ fontSize: "1.2em" }} />
+              </a>
             </li>
           </ul>
-        </nav>
-      </div>
-    </header>
+          {button && <button className='btn--outline'>
+            <a
+              href='https://github.com/amanrathore48'
+              onClick={closeMobileMenu}
+              target='_blank'
+              rel="noreferrer"
+            ><i className={`fab fa-github git-clr`}></i>
+            </a>
+          </button>}
+
+
+        </div>
+      </nav>
+    </>
   );
 }
+
+export default Navbar;
